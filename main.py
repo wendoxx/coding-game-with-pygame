ae3a88cfe0d20e2c0c8d0d6b75d49c24f35b1bcf
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-12-02 11:28:06 -0300

    feat: add game, menu and update main.py

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("Quitting")
             pygame.quit()
             quit()
