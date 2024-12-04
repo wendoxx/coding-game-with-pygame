4ef323dc484182c5ab690ba0732f2d4fc40ee8c1
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-12-04 11:43:37 -0300

    refactor: change event quit method from Game class to Menu class

--- code/Game.py
+++ code/Game.py
@@ -1,20 +1,16 @@
 import pygame
 
 from code.Menu import Menu
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self, ):
+    def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    print("Quitting")
-                    pygame.quit()
-                    quit()
